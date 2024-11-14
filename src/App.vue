f05ea3aa3911d5f9fab5c94be4794a36255a9218
Gia Huy Tong <gia.huy.tong1@gmail.com>
2024-11-14 17:03:45 +0100

    App.Vue ueberarbeitet

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
-  <div id="app">
-    <LebensmittelListe />
-    <KalorienCounter />
+  <div>
+    <div class="lebensmittelliste">
+      <h2>Lebensmittelliste</h2>
+      <ul>
+        <li v-for="lebensmittel in lebensmittelListe" :key="lebensmittel.name">
+          {{ lebensmittel.name }} - {{ lebensmittel.kalorien }} kcal
+        </li>
+      </ul>
+    </div>
+    <div class="gesamtkalorien">
+      <h2>Gesamtkalorien</h2>
+      <p>{{ totalKalorien }} kcal</p>
+    </div>
   </div>
 </template>
 
 <script>
-import LebensmittelListe from './components/LebensmittelListe.vue';
-import KalorienCounter from './components/KalorienCounter.vue';
-
 export default {
-  components: {
-    LebensmittelListe,
-    KalorienCounter,
+  data() {
+    return {
+      lebensmittelListe: [
+        { name: "Apfel", kalorien: 52 },
+        { name: "Banane", kalorien: 89 },
+        { name: "Kartoffel", kalorien: 77 },
+      ],
+    };
+  },
+  computed: {
+    totalKalorien() {
+      return this.lebensmittelListe.reduce((sum, item) => sum + item.kalorien, 0);
+    },
   },
 };
 </script>
+
+<style scoped>
+.lebensmittelliste, .gesamtkalorien {
+  margin-bottom: 20px;
+}
+.lebensmittelliste {
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+</style>

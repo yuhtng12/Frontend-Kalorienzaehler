a6885d2eab6a23a51a374b5eaa104aa5419f36bc
yuhtng12 <gia.huy.tong1@gmail.com>
2024-12-16 15:21:45 +0100

    Index.ts geändert

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import FoodListView from '@/views/FoodListView.vue';
+import TotalCaloriesView from '@/views/TotalCaloriesView.vue';
+
+const routes = [
+  { path: '/food-list', name: 'FoodList', component: FoodListView },
+  { path: '/calorie-overview', name: 'CalorieOverview', component: TotalCaloriesView },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  routes,
+});
 
-export default router
+export default router;
